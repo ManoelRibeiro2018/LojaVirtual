8a41eab2b4206154b374cfa05229ab6c9b90b22d
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-09-27 23:41:55 -0300

    Autenticando Colaborador

--- LojaVirtual/Interface/ICollaboratorService.cs
+++ LojaVirtual/Interface/ICollaboratorService.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using LojaVirtual.Models;
+using LojaVirtual.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LojaVirtual.Interface
 {
-    public class ICollaboratorService
+    public interface ICollaboratorService
     {
-
+        LoginViewModel Login(string email, string password);
+        int Insert(Collaborator collaborator);
+        void Update(int id, Collaborator collaborator);
+        void Delete(int id);
     }
 }
---
+++ LojaVirtual/Service/CollaboratorService.cs
@@ -0,0 +1,46 @@
+﻿using LojaVirtual.Interface;
+using LojaVirtual.Models;
+using LojaVirtual.Repository.Interface;
+using LojaVirtual.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Service
+{
+    public class CollaboratorService : ICollaboratorService
+    {
+        private readonly IAuthService _authService;
+        private readonly ICollaboratorRepository  _collaboratorRepository;
+
+        public CollaboratorService(IAuthService authService, ICollaboratorRepository  collaboratorRepository)
+        {
+            _authService = authService;
+            _collaboratorRepository = collaboratorRepository;
+        }
+        public LoginViewModel Login(string email, string password)
+        {
+            var passwordHash = _authService.ComputeSha256Hash(password);
+            var collaborator = _collaboratorRepository.Login(email, passwordHash);
+            var token = _authService.GenerateJwtToken(email, collaborator.Type);
+            return new LoginViewModel(collaborator.Email, token);
+
+        }
+
+        public int Insert(Collaborator collaborator)
+        {
+            throw new NotImplementedException();
+        }       
+
+        public void Update(int id, Collaborator collaborator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
