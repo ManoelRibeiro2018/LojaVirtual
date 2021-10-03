105b7dc6e4cac74ca46571d467dced426f0a88d0
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-10-02 22:55:29 -0300

    Criando entidade Category com Auto-relacionamento

---
+++ LojaVirtual/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int? IdCategoryFather { get; set; }
+
+        [ForeignKey("CategoryFather")]
+        public virtual Category CategoryFather { get; set; }
+    }
+}
